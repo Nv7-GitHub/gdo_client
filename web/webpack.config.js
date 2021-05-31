743b2d90c332b16515ddea6c31ad73976896ecd2
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2021-05-31 14:40:13 -0700

    Set up typescript

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,14 +1,29 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  devServer: {
-    publicPath: "/",
-    contentBase: "./dist",
-    hot: true
-  },
+module.exports = (env, argv) => {
+  return {
+    entry: './src/index.ts',
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+      publicPath: "/",
+      contentBase: "./dist",
+      hot: true,
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    devtool: argv.mode == "production" ? false : 'source-map',
+  }
 };
