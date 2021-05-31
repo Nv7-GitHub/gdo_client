4142f252f15c19d85371b2dbbe98009b09b12e5f
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2021-05-31 14:42:44 -0700

    update gitignore

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,7 +1,6 @@
 function component() {
   const element = document.createElement('div');
 
-  // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = "Hello webpack!"
 
   return element;
